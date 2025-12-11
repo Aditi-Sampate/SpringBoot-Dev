e493b7e567001a575ec583436f8de2cff96398d5
Aditi-Sampate <aditisampate@gmail.com>
2025-12-11 17:42:21 +0530

    Implemented the Factory Design Pattern for Dynamic User-Based Object Creation

---
+++ DocumentManagement-1/src/main/java/com/example/demo/Service/DocumentServiceAB.java
@@ -0,0 +1,13 @@
+package com.example.demo.Service;
+
+public abstract class DocumentServiceAB implements DocumentService{
+
+	@Override
+	public void readData() {
+		// TODO Auto-generated method stub
+		System.out.println("Reading data from Database");
+		
+	}
+
+
+}
