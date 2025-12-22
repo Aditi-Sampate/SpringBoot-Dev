399aeda0e61e766bfa82480cf5ce2b63dbcba188
Aditi-Sampate <aditisampate@gmail.com>
2025-12-22 18:01:16 +0530

    Added MediaFactory and MediaService Implementations ( Audio, Vedio, LiveStream) By Using Factory Design Template

---
+++ DesingPattern/FactoryDesingPattern/MediaPlayerSystem/src/main/java/com/example/demo/serviceImpl/AudioMediaServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.serviceImpl;
+
+import com.example.demo.service.MediaService;
+
+public class AudioMediaServiceImpl implements MediaService{
+
+	@Override
+	public String playMedia() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ DesingPattern/FactoryDesingPattern/MediaPlayerSystem/src/main/java/com/example/demo/serviceImpl/LiveStreamMediaServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.serviceImpl;
+
+import com.example.demo.service.MediaService;
+
+public class LiveStreamMediaServiceImpl implements MediaService{
+
+	@Override
+	public String playMedia() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
