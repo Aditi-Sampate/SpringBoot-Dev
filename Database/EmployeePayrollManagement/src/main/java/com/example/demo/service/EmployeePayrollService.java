148489152cf476df75e638d85a6d44ee88da2c13
Aditi-Sampate <aditisampate@gmail.com>
2026-01-05 22:35:38 +0530

    Create Employee Payroll Management Database with Curd Operations

---
+++ Database/EmployeePayrollManagement/src/main/java/com/example/demo/service/EmployeePayrollService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.EmployeePayroll;
+
+public interface EmployeePayrollService {
+
+	void addEmployee(EmployeePayroll employeePayroll);
+	
+	void deleteEmployee(int id);
+	
+	EmployeePayroll getEmployee(int id);
+}
