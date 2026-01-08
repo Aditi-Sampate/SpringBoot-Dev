d3d570e906f1b4b33036c56523c0bbe384d19ac3
Aditi-Sampate <aditisampate@gmail.com>
2026-01-08 21:26:48 +0530

    Implemented The Concept of Database in Inventory Management Project

---
+++ Database/InventoryManagement/src/main/java/com/example/demo/service/InventoryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Inventor;
+import com.example.demo.repository.InventoryRepository;
+
+@Service
+public class InventoryServiceImpl implements InventoryService{
+
+	@Autowired
+	InventoryRepository inventoryRepository;
+	
+	@Override
+	public void addProduct(Inventor inventor) {
+		// TODO Auto-generated method stub
+		inventoryRepository.save(inventor);
+	}
+
+	@Override
+	public void deleteProduct(int id) {
+		// TODO Auto-generated method stub
+		inventoryRepository.deleteById(id);	
+	
+	}
+
+	@Override
+	public Inventor getProduct(int id) {
+		// TODO Auto-generated method stub
+		return inventoryRepository.findById(id).get();
+	}
+
+}
