90b0434a6fb98c24c62f658d459cf7a601b2b9a5
Aditi-Sampate <aditisampate10@gmail.com>
2026-02-22 14:31:14 +0530

    Added One Number List by Using ArrayList And From That list find how Many numbers are even or how many numbers are odd.

---
+++ Collection/01_List_Interface/01_ArrayList/ArrayList2/src/EvenOddArrayList.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class EvenOddArrayList {
+
+public static void main(String[] args) {
+
+	ArrayList<Integer> numbers = new ArrayList<>();
+	
+	numbers.add(10);
+	numbers.add(15);
+	numbers.add(22);
+	numbers.add(35);
+	numbers.add(40);
+	
+	System.out.println("Numbers :" +numbers);
+	
+	int eventCount = 0;
+	int oddCount =0;
+	
+	for(int i=0; i<numbers.size(); i++)
+	{
+		int num = numbers.get(i);
+		
+		if(num % 2 ==0)
+		{
+			eventCount++;
+		}
+		else
+		{
+			oddCount++;
+		}
+	}
+	System.out.println("Even Numbers Count :" +eventCount);
+	System.out.println("Odd Numbers Count :"+oddCount);
+	
+ }
+
+}
