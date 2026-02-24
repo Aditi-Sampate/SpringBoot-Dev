84dfa18c259cca9dcad337465002cdfff125c5dd
Aditi-Sampate <aditisampate10@gmail.com>
2026-02-24 23:09:42 +0530

    Add Vector Program To Update Element at Specific Index Using Set Method.

---
+++ Collection/01_List_Interface/03_Vector/VectorList3/src/UpdateVector.java
@@ -0,0 +1,20 @@
+import java.util.Vector;
+
+public class UpdateVector {
+
+	public static void main(String[] args) {
+		
+		Vector<Integer> marks = new Vector<>();
+		
+		marks.add(70);
+		marks.add(80);
+		marks.add(90);
+		
+		System.out.println("Before Update :"+marks);
+		
+		marks.set(1, 85);	//Update index 1
+		
+		System.out.println("After Update :"+marks);
+		
+	}
+}
